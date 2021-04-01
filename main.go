07e00cdbafb304150fffee58a918dd8ac3353d72
Purp1e <47248616+Purple-CSGO@users.noreply.github.com>
2021-04-01 18:06:10 +0800

    样式修改4

--- main.go
+++ main.go
@@ -4,17 +4,13 @@ import (
 	"One-Encoder/backend"
 	"github.com/leaanthony/mewn"
 	"github.com/wailsapp/wails"
-	"runtime"
 )
 
 func main() {
 	//TODO 启动前读取CFG，包括w, h, scale
 
-	w, h := 660, 408-14
+	w, h := 660, 394
 	//无边框窗口特性 wails v2.0之前解决固定窗口高度问题 TODO linux 高度测试
-	if runtime.GOOS == "darwin" {
-		//h += 28
-	}
 
 	//绑定js和css
 	js := mewn.String("./frontend/dist/app.js")
